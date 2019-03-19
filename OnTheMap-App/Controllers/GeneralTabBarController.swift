1278ed88c33d7337f0c55c188b7be30172d322bc
ImYazeed <imYazeedALZahrani@gmail.com>
2019-03-19 19:31:59 +0300

    Add Activity Indicator

--- OnTheMap-App/Controllers/GeneralTabBarController.swift
+++ OnTheMap-App/Controllers/GeneralTabBarController.swift
@@ -9,47 +9,71 @@
 import UIKit
 
 class GeneralTabBarController: UITabBarController {
-
+    
+    var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupActivityIndicator()
         getStudentInformation()
-        
     }
     
-    
+
     @IBAction func logOutTapped(_ sender: Any) {
         
         UdacityClient.logout(sucssess: {
-        self.dismiss(animated: true, completion: nil)
+            self.dismiss(animated: true, completion: nil)
         }) { (error) in
             
-             self.showErrorAlert(messege: error.localizedDescription)
-        }
-    }
-    
-    func getStudentInformation() {
-        
-        ParseClient.getStudentsLocation(success: { (studentsInformationResults) in
-            StudentInformationModel.results = studentsInformationResults
-        }) { (error) in
             self.showErrorAlert(messege: error.localizedDescription)
         }
     }
     
+    @IBAction func refershTapped(_ sender: Any) {
+        getStudentInformation()
+    }
+    func getStudentInformation() {
+        
+        performRefereshIndicator()
+        ParseClient.getStudentsLocation(success: { (studentsInformationResults) in
+            StudentInformationModel.results = studentsInformationResults
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "studuntInformationUpdated"), object: nil)
+           self.performRefereshIndicator()
+        }) { (error) in
+            self.performRefereshIndicator()
+            self.showErrorAlert(messege: error.localizedDescription)
+            
+        }
+    }
+    
+    func setupActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView(style: .gray)
+        activityIndicator.center = self.view.center
+        self.view.addSubview(activityIndicator)
+    }
+    
+    func performRefereshIndicator() {
+        
+        if activityIndicator.isAnimating {
+            activityIndicator.stopAnimating()
+        }else {
+            activityIndicator.startAnimating()
+        }
+        
+    }
     
     func showErrorAlert(messege: String) {
-
+        
         AlertManager.showLoginFailureFromViewController(viewController: self, message: messege)
+        
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- OnTheMap-App/Controllers/MapViewController.swift
+++ OnTheMap-App/Controllers/MapViewController.swift
@@ -12,22 +12,21 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
     
-    
+    @objc func configureActivityIndicator() {
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    override func viewWillDisappear(_ animated: Bool) {
+     
+    }
 
 }
